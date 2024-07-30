0bbe66d90925ee36911e437a475d28f781fd8b80
CarlNxumalo <carl.ybongani@gmail.com>
2024-07-30 12:20:13 +0200

    Hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Section, News, HeroHeader, HeroBody } from 'flowbite-svelte-blocks';
+ 
+</script>
+
+<Section name="heroDefault">
+  <News>
+    <span class="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span>
+    <span class="text-sm font-medium">Flowbite is out! See what's new</span>
+  </News>
+  <HeroHeader>
+    <svelte:fragment slot="h1">We invest in the world’s potential</svelte:fragment>
+    <svelte:fragment slot="paragraph">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</svelte:fragment>
+  </HeroHeader>
+</Section>
