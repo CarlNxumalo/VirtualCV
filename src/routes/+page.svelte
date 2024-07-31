2ea85796c71f75689d0e7861d023c3ef235987a1
CarlNxumalo <carl.ybongani@gmail.com>
2024-07-31 22:30:20 +0200

    Okay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
   import { Label, Input, Textarea } from 'flowbite-svelte';
   import {  FeatureDefault, FeatureItem } from 'flowbite-svelte-blocks';
   import { ChartPieSolid, LandmarkSolid, BriefcaseSolid, DollarOutline, RocketSolid, CogOutline } from 'flowbite-svelte-icons';
+  import { Footer, FooterBrand, FooterLinkGroup, FooterLink, FooterCopyright, FooterIcon } from 'flowbite-svelte';
 </script>
 
 <Section name="heroDefault">
@@ -194,3 +195,17 @@
       </form>
     </Contact>
   </Section>
+
+
+  
+  <Footer footerType="socialmedia">
+    <div class="mx-auto max-w-screen-xl text-center">
+      <FooterBrand href="https://flowbite.com" src="/images/logo.svg" alt="Flowbite Logo" name="Flowbite" aClass="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white" />
+      <p class="my-6 text-gray-500 dark:text-gray-400">Open-source library of over 400+ web components and interactive elements built for better web.</p>
+      <FooterLinkGroup ulClass="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+        <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/">Github</FooterLink>
+        <FooterLink liClass="" aClass="mr-4 hover:underline md:mr-6" href="/">LinkedIn</FooterLink>
+      </FooterLinkGroup>
+      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2021-2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+    </div>
+  </Footer>
